e4bf650f46531faa380cbcca4beb996d2a5e2e4b
Nelson Esmarlin Rosario Rodríguez <esmarlin9@gmail.com>
2024-02-19 23:54:19 -0400

    improve: Separate files logic

--- Paginator.cs
+++ Paginator.cs
@@ -129,32 +129,4 @@ namespace CorePagination
         public string LastPageUrl { get; set; }
     }
 
-
-    public static class Guard
-    {
-        public static void NotNull<T>(T argument, string paramName) where T : class
-        {
-            if (argument == null)
-            {
-                throw new ArgumentNullException(paramName, "Argument cannot be null.");
-            }
-        }
-
-        public static void NotNegative(int value, string paramName)
-        {
-            if (value < 0)
-            {
-                throw new ArgumentOutOfRangeException(paramName, "Argument cannot be negative.");
-            }
-        }
-
-        public static void NotNullOrWhiteSpace(string argument, string paramName)
-        {
-            if (string.IsNullOrWhiteSpace(argument))
-            {
-                throw new ArgumentException("Argument cannot be null or whitespace.", paramName);
-            }
-        }
-    }
-
 }
