198b46308f71825cf6e1699de372c3653ad13a4c
Nelson Esmarlin Rosario Rodríguez <esmarlin9@gmail.com>
2024-02-24 00:22:30 -0400

    improve: Improve extensions methos for pagination

--- CorePagination/Extensions/PaginatorExtensions.cs
+++ CorePagination/Extensions/PaginatorExtensions.cs
@@ -3,6 +3,7 @@ using CorePagination.Paginators.Common;
 using CorePagination.Paginators.CursorPaginator;
 using CorePagination.Paginators.SimplePaginator;
 using CorePagination.Paginators.SizeAwarePaginator;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,17 +18,31 @@ namespace CorePagination.Extensions
         public static async Task<PaginationResult<T>> SimplePaginateAsync<T>(
             this IQueryable<T> query, int pageNumber, int pageSize)
         {
-            var paginator = new SimplePaginator<T>();
-            var parameters = new PaginatorParameters { PageSize = pageSize, Page = pageNumber };
-            return await paginator.PaginateAsync(query, parameters);
+            var items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginationResult<T>
+            {
+                Items = items,
+                PageSize = pageSize,
+                Page = pageNumber,
+                TotalItems = null
+            };
         }
 
         public static async Task<SizeAwarePaginationResult<T>> PaginateAsync<T>(
-            this IQueryable<T> query, int pageNumber , int pageSize)
+            this IQueryable<T> query, int pageNumber, int pageSize)
         {
-            var paginator = new SizeAwarePaginator<T>();
-            var parameters = new PaginatorParameters { PageSize = pageSize, Page = pageNumber };
-            return await paginator.PaginateAsync(query, parameters);
+            var items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+            var totalItems = await query.CountAsync();
+            var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
+            return new SizeAwarePaginationResult<T>
+            {
+                Items = items,
+                PageSize = pageSize,
+                Page = pageNumber,
+                TotalItems = totalItems,
+                TotalPages = totalPages
+            };
         }
 
         public static async Task<CursorPaginationResult<T, TKey>> CursorPaginateAsync<T, TKey>(
