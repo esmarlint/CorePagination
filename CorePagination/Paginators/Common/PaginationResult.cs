7a75d27f1dabb0374fde84054fb98db4c8a3efa6
Nelson Esmarlin Rosario Rodríguez <esmarlin9@gmail.com>
2024-03-07 20:50:12 -0400

    documentation: Add XML documentation comments to pagination parameter classes and their properties

--- CorePagination/Paginators/Common/PaginationResult.cs
+++ CorePagination/Paginators/Common/PaginationResult.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 
 namespace CorePagination.Paginators.Common
 {
+    /// <summary>
+    /// Represents the result of a basic pagination operation.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the pagination result.</typeparam>
     public class PaginationResult<T> : IPaginationResult<T>
     {
         public IEnumerable<T> Items { get; set; }
--- CorePagination/Paginators/CursorPaginator/CursorPaginationParameters.cs
+++ CorePagination/Paginators/CursorPaginator/CursorPaginationParameters.cs
@@ -8,9 +8,19 @@ using System.Threading.Tasks;
 
 namespace CorePagination.Paginators.CursorPaginator
 {
+    /// <summary>
+    /// Represents the parameters for cursor-based pagination.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the cursor property.</typeparam>
     public class CursorPaginationParameters<TKey> : PagePaginatorParameters
     {
+        /// <summary>
+        /// Gets or sets the current cursor value.
+        /// </summary>
         public TKey CurrentCursor { get; set; }
+        /// <summary>
+        /// Gets or sets the pagination order (ascending or descending). Default is ascending.
+        /// </summary>
         public PaginationOrder Order { get; set; } = PaginationOrder.Ascending;
     }
 }
