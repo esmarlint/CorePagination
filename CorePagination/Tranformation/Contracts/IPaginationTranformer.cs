96213276b7e0e94c39048c061c15f9ba81269713
Nelson Esmarlin Rosario Rodríguez <esmarlin9@gmail.com>
2024-03-07 20:40:09 -0400

    documentation: Add XML documentation comments to IPaginationTransformer interface and its Transform method

--- CorePagination/Tranformation/Contracts/IPaginationTranformer.cs
+++ CorePagination/Tranformation/Contracts/IPaginationTranformer.cs
@@ -2,10 +2,20 @@
 
 namespace CorePagination.Tranformation.Contracts
 {
+    /// <summary>
+    /// Defines the contract for transforming a pagination result into a different format.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the original pagination result.</typeparam>
+    /// <typeparam name="TResult">The type of the transformed pagination result.</typeparam>
     public interface IPaginationTranformer<T, TResult>
         where T : class
         where TResult : class
     {
+        /// <summary>
+        /// Transforms the specified pagination result into a different format.
+        /// </summary>
+        /// <param name="paginationResult">The pagination result to be transformed.</param>
+        /// <returns>The transformed pagination result.</returns>
         TResult Transform(IPaginationResult<T> paginationResult);
     }
 
