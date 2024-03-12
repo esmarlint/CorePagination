ea3c79fb9dbcbf817829572ba091f06670c4103c
Nelson Esmarlin Rosario Rodríguez <esmarlin9@gmail.com>
2024-03-11 20:52:49 -0400

    feature: Add WithSimpleUrl extension method for SimpleUrlResultTransformer,CursorUrlResultTransformer and SizeAwareUrlResultTransformer

---
+++ CorePagination/Tranformation/Extensions/PaginationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using CorePagination.Contracts;
+using CorePagination.Paginators.Common;
+using CorePagination.Tranformation.Transformers;
+
+namespace CorePagination.Tranformation.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static UrlPaginationResult<T> WithSimpleUrl<T>(this IPaginationResult<T> paginationResult, string baseUrl)
+                 where T : class
+        {
+            var transformer = new SimpleUrlResultTransformer<T>(baseUrl);
+            return transformer.Transform(paginationResult);
+        }
+
+        public static UrlPaginationResult<T> WithUrl<T>(this IPaginationResult<T> paginationResult, string baseUrl)
+            where T : class
+        {
+            var transformer = new SizeAwareUrlResultTransformer<T>(baseUrl);
+            return transformer.Transform(paginationResult);
+        }
+
+        public static CursorUrlPaginationResult<T, TKey> WithUrl<T, TKey>(
+            this IPaginationResult<T> paginationResult, string baseUrl)
+            where T : class
+            where TKey : IComparable
+        {
+            var transformer = new CursorUrlResultTranformer<T, TKey>(baseUrl);
+            return (CursorUrlPaginationResult<T, TKey>)transformer.Transform(paginationResult);
+        }
+    }
+}
