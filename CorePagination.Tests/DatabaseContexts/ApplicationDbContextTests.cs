ad7d79dec20a60055ae65b0b21bb6283ce967ccf
Nelson Esmarlin Rosario Rodríguez <esmarlin9@gmail.com>
2024-03-15 22:11:15 -0400

    feature: Configure contexts and tests files

---
+++ CorePagination.Tests/DatabaseContexts/ApplicationDbContextTests.cs
@@ -0,0 +1,20 @@
+﻿using CorePagination.Tests.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePagination.Tests.DatabaseContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductTets> Products { get; set; }
+    }
+}
