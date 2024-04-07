4c03b52d933d86bb81098de47c11a990afff6f46
Nelson Esmarlin Rosario Rodríguez <esmarlin9@gmail.com>
2024-04-06 22:15:53 -0400

    enhancement: Add unit tests for CursorPaginate method

--- CorePagination.Tests/Extensions/PaginatorExtensionsTests.cs
+++ CorePagination.Tests/Extensions/PaginatorExtensionsTests.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using FakeDbContext = CorePagination.Tests.DatabaseContexts.ApplicationDbContext;
@@ -22,9 +23,9 @@ namespace CorePagination.Tests.Extensions
                 .Options;
         }
 
-        private static void SeedTestData(FakeDbContext context)
+        private static void SeedTestData(FakeDbContext context,int amount = 30)
         {
-            ProductSeeder.SeedProducts(context, 30);
+            ProductSeeder.SeedProducts(context, amount);
             context.SaveChanges();
         }
 
@@ -102,5 +103,132 @@ namespace CorePagination.Tests.Extensions
         }
 
         #endregion SimplePaginate
+
+        #region Paginate
+
+        [Fact]
+        public void Paginate_ShouldThrowArgumentNullException_WhenQueryIsNull()
+        {
+            var pageNumber = 1;
+            var pageSize = 10;
+
+            Assert.Throws<ArgumentNullException>(() => PaginatorExtensions.Paginate<object>(null, pageNumber, pageSize));
+        }
+
+        [Theory]
+        [InlineData(0, 10)]
+        [InlineData(1, 0)]
+        public void Paginate_ShouldThrowArgumentOutOfRangeException_WhenPageNumberOrPageSizeIsInvalid(int pageNumber, int pageSize)
+        {
+            var query = new List<object>().AsQueryable();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => query.Paginate(pageNumber, pageSize));
+        }
+
+        [Fact]
+        public void Paginate_ShouldPaginateCorrectly()
+        {
+            var options = CreateInMemoryDatabaseOptions();
+            using var context = new FakeDbContext(options);
+            SeedTestData(context,20);
+            var data = context.Products;
+            var pageNumber = 2;
+            var pageSize = 10;
+
+            var result = data.Paginate(pageNumber, pageSize);
+
+            Assert.Equal(pageSize, result.Items.Count());
+            Assert.Equal(20, result.TotalItems);
+            Assert.Equal(2, result.TotalPages);
+            Assert.Equal(11, result.Items.First().Id);
+        }
+
+        [Fact]
+        public async Task PaginateAsync_ShouldThrowArgumentNullException_WhenQueryIsNull()
+        {
+            var pageNumber = 1;
+            var pageSize = 10;
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => PaginatorExtensions.PaginateAsync<object>(null, pageNumber, pageSize));
+        }
+
+        [Theory]
+        [InlineData(0, 10)]
+        [InlineData(1, 0)]
+        public async Task PaginateAsync_ShouldThrowArgumentOutOfRangeException_WhenPageNumberOrPageSizeIsInvalid(int pageNumber, int pageSize)
+        {
+            var query = new List<object>().AsQueryable();
+
+            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(() => query.PaginateAsync(pageNumber, pageSize));
+        }
+
+        [Fact]
+        public async Task PaginateAsync_ShouldPaginateCorrectly()
+        {
+            var options = CreateInMemoryDatabaseOptions();
+            using var context = new FakeDbContext(options);
+            SeedTestData(context, 50);
+            var data = context.Products;
+            var pageNumber = 2;
+            var pageSize = 10;
+
+            var result = await data.PaginateAsync(pageNumber, pageSize);
+
+            Assert.Equal(pageSize, result.Items.Count());
+            Assert.Equal(50, result.TotalItems);
+            Assert.Equal(5, result.TotalPages);
+            Assert.Equal(11, result.Items.First().Id);
+        }
+
+
+        #endregion Paginate
+
+        #region CursorPaginate
+        [Fact]
+        public void CursorPaginate_ShouldThrowArgumentNullException_WhenQueryIsNull()
+        {
+            Expression<Func<ProductTests, int>> keySelector = x => x.Id;
+            var pageSize = 10;
+            var currentCursor = 1;
+
+            Assert.Throws<ArgumentNullException>(() => PaginatorExtensions.CursorPaginate<ProductTests, int>(null, keySelector, pageSize, currentCursor));
+        }
+
+        [Fact]
+        public void CursorPaginate_ShouldThrowArgumentNullException_WhenKeySelectorIsNull()
+        {
+            var query = new List<ProductTests>().AsQueryable();
+            var pageSize = 10;
+            var currentCursor = 1;
+
+            Assert.Throws<ArgumentNullException>(() => query.CursorPaginate<ProductTests, int>(null, pageSize, currentCursor));
+        }
+
+        [Fact]
+        public void CursorPaginate_ShouldThrowArgumentOutOfRangeException_WhenPageSizeIsInvalid()
+        {
+            var query = new List<ProductTests>().AsQueryable();
+            Expression<Func<ProductTests, int>> keySelector = x => x.Id;
+            var currentCursor = 1;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => query.CursorPaginate(keySelector, 0, currentCursor));
+        }
+
+        [Fact]
+        public void CursorPaginate_ShouldPaginateCorrectly()
+        {
+            var data = Enumerable.Range(1, 50).Select(x => new ProductTests { Id = x }).AsQueryable();
+            Expression<Func<ProductTests, int>> keySelector = x => x.Id;
+            var pageSize = 10;
+            var currentCursor = 20;
+
+            var result = data.CursorPaginate(keySelector, pageSize, currentCursor);
+
+            Assert.Equal(pageSize, result.Items.Count());
+            Assert.Equal(21, result.Items.First().Id);
+        }
+
+        #endregion CursorPaginate
+
     }
 }
