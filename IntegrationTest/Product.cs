cf21fb3c9b0953bba9670edaf3a783c39ba55062
Nelson Esmarlin Rosario Rodríguez <esmarlin9@gmail.com>
2024-05-01 23:09:45 -0400

    feature: Update accessibility

--- IntegrationTest/Product.cs
+++ IntegrationTest/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CorePagination.Benchmarks
 {
-    internal class Product
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
