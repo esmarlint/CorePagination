f5cf0e54a15ddf2761f8f710946c9735b3ad22fe
Nelson Esmarlin Rosario Rodríguez <esmarlin9@gmail.com>
2024-05-04 23:36:54 -0400

    feature: Add tranformers benchmarks

--- IntegrationTest/PaginatorBenchmarks.cs
+++ IntegrationTest/PaginatorBenchmarks.cs
@@ -4,6 +4,8 @@ using CorePagination.Paginators.Common;
 using CorePagination.Paginators.CursorPaginator;
 using CorePagination.Paginators.SimplePaginator;
 using CorePagination.Paginators.SizeAwarePaginator;
+using CorePagination.Tranformation.Transformers;
+using CorePagination.Tranformation.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace CorePagination.Benchmarks
@@ -165,6 +167,76 @@ namespace CorePagination.Benchmarks
             var query = _context.Products.Take(totalItems).OrderBy(p => p.Id);
             var parameters = new CursorPaginationParameters<int> { PageSize = pageSize };
             var result = _cursorPaginator.Paginate(query, parameters);
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class TransformersBenchmarks
+    {
+        private BenchmarkDbContext _context;
+        private SizeAwarePaginator<Product> _sizeAwarePaginator;
+        private SimplePaginator<Product> _simplePaginator;
+        private CursorPaginator<Product, int> _cursorPaginator;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _context = new BenchmarkDbContext();
+
+            var products = Enumerable.Range(1, 10000)
+                .Select(i => new Product { Id = i, Name = $"Product {i}" })
+                .ToList();
+
+            _context.Products.AddRange(products);
+            _context.SaveChanges();
+
+            _sizeAwarePaginator = new SizeAwarePaginator<Product>();
+            _simplePaginator = new SimplePaginator<Product>();
+            _cursorPaginator = new CursorPaginator<Product, int>(p => p.Id);
+        }
+
+        [Benchmark]
+        [Arguments(100, 10)]
+        [Arguments(1000, 20)]
+        public async Task SizeAwarePaginatorWithTransformerAsync(int totalItems, int pageSize)
+        {
+            var query = _context.Products.Take(totalItems);
+            var parameters = new PaginatorParameters { Page = 1, PageSize = pageSize };
+            var paginationResult = await _sizeAwarePaginator.PaginateAsync(query, parameters);
+            var transformedResult = new SizeAwareUrlResultTransformer<Product>("/products").Transform(paginationResult);
+        }
+
+        [Benchmark]
+        [Arguments(100, 10)]
+        [Arguments(1000, 20)]
+        public async Task SizeAwarePaginatorWithTransformerExtensionAsync(int totalItems, int pageSize)
+        {
+            var query = _context.Products.Take(totalItems);
+            var parameters = new PaginatorParameters { Page = 1, PageSize = pageSize };
+            var paginationResult = await _sizeAwarePaginator.PaginateAsync(query, parameters);
+            var transformedResult = paginationResult.WithUrl("/products");
+        }
+
+        [Benchmark]
+        [Arguments(100, 10)]
+        [Arguments(1000, 20)]
+        public async Task CursorPaginatorWithTransformerAsync(int totalItems, int pageSize)
+        {
+            var query = _context.Products.Take(totalItems).OrderBy(p => p.Id);
+            var parameters = new CursorPaginationParameters<int> { PageSize = pageSize };
+            var paginationResult = await _cursorPaginator.PaginateAsync(query, parameters);
+            var transformedResult = new CursorUrlResultTransformer<Product, int>("/products").Transform(paginationResult);
+        }
+
+        [Benchmark]
+        [Arguments(100, 10)]
+        [Arguments(1000, 20)]
+        public async Task CursorPaginatorWithTransformerExtensionAsync(int totalItems, int pageSize)
+        {
+            var query = _context.Products.Take(totalItems).OrderBy(p => p.Id);
+            var parameters = new CursorPaginationParameters<int> { PageSize = pageSize };
+            var paginationResult = await _cursorPaginator.PaginateAsync(query, parameters);
+            var transformedResult = paginationResult.WithUrl<Product, int>("/products");
         }
     }
 }
